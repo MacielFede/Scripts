bd5d132b2264b6239d8a30cbde9ff892abc9187f
MacielFede <federicomaciel.dev@gmail.com>
2023-12-08 09:59:17 -0300

    Add files via upload

--- gitFetchout.sh
+++ gitFetchout.sh
@@ -3,12 +3,12 @@ echo -e "\033[32m -> I'm going to fetch the indicated branch from the set origin
 
 read -p $'\e[32m -> Insert the branch you want to checkout to here: \e[0m: ' branch
 
-git pull origin branch
+git pull origin "$branch"
 if [ $? -ne 0 ]; then 
   echo -e "\033[31m -> There's a problem with the pull :(\033[0m"
   exit 1 
 else 
-  git checkout -f branch
+  git checkout -f "$branch"
   if [ $? -ne 0 ]; then 
     echo -e "\033[31m -> There's a problem with the checkout :( Debug the problem and try again later.\033[0m"
     exit 1
